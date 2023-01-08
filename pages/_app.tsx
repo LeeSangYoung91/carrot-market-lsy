57c74d1f7941793941a79da6c703246cf5351576
“LeeSangYoung91” <zxcdasd3@gmail.com>
2023-01-08 20:23:22 +0900

    0108 1차커밋

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,25 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { SWRConfig } from "swr";   //swr전역 설정 
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     // 여기로 피시용 모바일용 제어
+    
+    <SWRConfig
+    value={{
+      //refreshInterval:2000  2000ms마다 새로고침을 하고싶을때 
+      fetcher: (url: string) =>
+        fetch(url).then((response) => response.json()),
+    }}
+  >
     <div className="w-full max-w-xl mx-auto">
       <Component {...pageProps} />
     </div>
-  );
+
+  </SWRConfig>
+);
 }
+
 
 export default MyApp;
